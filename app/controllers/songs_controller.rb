ea3f41b32a8c2ab84d547a570b92566a847c59d5
thesilentc <thesilentc@me.com>
2018-06-25 23:07:38 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -40,8 +40,8 @@ class SongsController < ApplicationController
       @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
       @song.genre_ids = params[:genres]
       @song.save
-      flash[:message] = "Successfully updated song."
 
+      flash[:message] = "Successfully updated song."
       redirect to("/songs/#{@song.slug}")
     end
 
